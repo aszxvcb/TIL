36b3addc562639953a5a59a4ab53e003073e822c
SangHyeon <aszxvcb@gmail.com>
2020-12-26 05:51:10 +0900

    MOD lv1_크레인_인형뽑기.cpp
     - 포인터 대신 참조자(&) 사용

--- 프로그래머스/lv1_크레인_인형뽑기.cpp
+++ 프로그래머스/lv1_크레인_인형뽑기.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int choose(vector<vector<int>>* board, int move); // 뽑기기계에서 인형을 뽑는 함수 
+int choose(vector<vector<int>>& board, int move); // 뽑기기계에서 인형을 뽑는 함수 
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
@@ -14,7 +14,7 @@ int solution(vector<vector<int>> board, vector<int> moves) {
     stack<int> s;
 
     for( int i=0; i<moves.size(); i++){
-        cur = choose(&board, moves[i]-1); // moves의 값이 1부터 시작하기 때문에 -1 필요
+        cur = choose(board, moves[i]-1); // moves의 값이 1부터 시작하기 때문에 -1 필요
         // printf("[check] %d", cur);
 
         if( cur == 0 ) continue;
@@ -26,7 +26,7 @@ int solution(vector<vector<int>> board, vector<int> moves) {
             if( s.top() != cur ){
                 s.push(cur);
             }
-            else{
+            else{ // 스택의 맨 위 인형과 현재 들어올 인형이 같다면, 스택에 푸쉬하지 않는다.
                 s.pop();
                 pop_cnt++;
             }
@@ -34,18 +34,19 @@ int solution(vector<vector<int>> board, vector<int> moves) {
     }
 
     answer = pop_cnt*2;
+    // printf("\n%d\n", answer);
 
     return answer;
 }
 
-int choose(vector<vector<int>>* board, int move){
-    /* vector 포인터 접근시 (*board) 하여 접근 */
+int choose(vector<vector<int>>& board, int move){
+    /* c++에서는 참조자를 사용할 수 있음. 될 수 있으면 포인터 대신 참조자를 사용한다. */
     int ret=0;
 
-    for ( int i=0; i<(*board)[move].size(); i++ ){
-        if( (*board)[i][move] != 0 ){
-            ret = (*board)[i][move];
-            (*board)[i][move] = 0;
+    for ( int i=0; i<(board)[move].size(); i++ ){
+        if( (board)[i][move] != 0 ){
+            ret = (board)[i][move];
+            (board)[i][move] = 0;
             return ret;
         }
     }
