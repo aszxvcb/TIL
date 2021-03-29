ea7a0e043e8292bae0ac0379b77953d5bbd69491
SangHyeon <aszxvcb@gmail.com>
2021-03-29 18:04:18 +0900

    ADD lv1_나누어_떨어지는_숫자_배열.cpp

---
+++ 프로그래머스/lv1_나누어_떨어지는_숫자_배열.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    
+    for(int elem : arr){
+        if( elem % divisor == 0){
+            answer.push_back(elem);
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    if ( answer.empty() ){
+        answer.push_back(-1);
+    }
+    return answer;
+}
